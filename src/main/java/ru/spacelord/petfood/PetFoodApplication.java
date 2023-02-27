598111103f824034303f9bd80a57836e1d71fe33
spacelord21 <incrediblespaceee@gmail.com>
2023-02-27 12:24:33 +0200

    Initial commit

---
+++ src/main/java/ru/spacelord/petfood/PetFoodApplication.java
@@ -0,0 +1,13 @@
+package ru.spacelord.petfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spacelord/petfood/PetFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spacelord.petfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
